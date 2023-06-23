17237c3788229e53807bfef749117c2234c9705c
Kameron Harvey <kam.harvey10@gmail.com>
2023-06-23 10:19:56 -0400

    Initial Commit

---
+++ projectOne/Preview Content/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  projectOne
+//
+//  Created by Scholar on 6/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        
+            
+            Text("Hi Kameron!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.white)
+            
+            Image("ME!!")
+                .resizable(resizingMode: .stretch)
+                .imageScale(.small)
+                .foregroundColor(.accentColor)
+            Text("I'm 17 and I'm a rising senior at North Cobb HS")
+                .foregroundColor(.white)
+
+        }
+        .padding()
+        .background(.purple)
+    
+        Button("More facts!!") {
+            
+        }
+    
+    
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
